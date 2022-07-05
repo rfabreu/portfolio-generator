6a828b99c35104009c631760d5fe4bf759e156b0
rfabreu <raabreugomes@gmail.com>
2022-07-05 11:08:14 -0400

    Copy CSS file using Promises

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -17,3 +17,18 @@ const writeFile = fileContent => {
         });
     });
 };
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'Style sheet copied successfully!'
+            });
+        });
+    });
+};
